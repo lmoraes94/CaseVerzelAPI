68b2735491c91260ba2725ed344c84747a291c1b
lmoraes94 <devleonardodemoraes@gmail.com>
2022-10-12 20:24:43 -0300

    update cors

--- src/app.ts
+++ src/app.ts
@@ -5,7 +5,7 @@ import routes from "./routes";
 
 const app = express();
 
-app.use(cors({ origin: ["http://localhost:3000"] }));
+app.use(cors({ origin: ["http://localhost:3000", "http://localhost:3001"] }));
 app.use(json());
 app.use(urlencoded({ extended: true }));
 
